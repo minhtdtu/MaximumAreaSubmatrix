777f63b956edd193987f8837adfa53090476a398
oxochenxa <oxochenxa@gmail.com>
2019-12-03 09:14:42 +0700

    add maximumsubmatrix

--- MaximumAreaSubmatrix.py
+++ MaximumAreaSubmatrix.py
@@ -1,3 +1,4 @@
+import re
 import sys
 def maximalRectangle(matrix,numRowCol):
     if not matrix:
@@ -6,7 +7,7 @@ def maximalRectangle(matrix,numRowCol):
     lookupTable = [0 for _ in range(numRowCol)]
     for row in range(numRowCol):
         for col in range(numRowCol):
-            # If it is "1" then sum column
+            # If it is "1"
             if int(matrix[row][col]) > 0:
                 lookupTable[col] += int(matrix[row][col])
             else:
@@ -18,16 +19,15 @@ def maximalRectangle(matrix,numRowCol):
 
 
 def maximalRectangleInHistogram(histogram):
-    # Create Stack for storing the index.
+    # Stack for storing the index.
     posStack = []
     i = 0
     maxArea = 0
     while i < len(histogram):
-        # if stack = None or current height is greater than the top one of the stack then append to posStack
         if len(posStack) == 0 or histogram[i] > histogram[posStack[-1]]:
             posStack.append(i)
             i += 1
-        else:   #Update maxArea
+        else:
             curr = posStack.pop()
             width = i if len(posStack) == 0 else i - posStack[-1] - 1
             maxArea = max(maxArea, width * histogram[curr])
@@ -37,9 +37,13 @@ def maximalRectangleInHistogram(histogram):
         width = i if len(posStack) == 0 else len(histogram) - posStack[-1] - 1
         maxArea = max(maxArea, width * histogram[curr])
     return maxArea
-# Create value and read file
+
+
+def debugPrint(historgram):
+    out = [i for i in historgram]
+    print(out)
 matrix = []
-numRowCol = int(open('in.txt', "r").readline()[0:])
+numRowCol = int(open(sys.argv[1], "r").readline()[0:])
 file = open(sys.argv[1], "r")
 readFile = file.readlines()[1:]
 for line in readFile:
@@ -48,9 +52,7 @@ for line in readFile:
     matrix.append(line)
 
 print(maximalRectangle(matrix,numRowCol))
-
 answer = maximalRectangle(matrix, numRowCol)
-# Write file
 with open(sys.argv[2], 'w') as f:
     f.write("%s " % answer)
     f.write('\n')
